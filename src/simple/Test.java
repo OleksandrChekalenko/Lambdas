77ffd7ee1b7e0a8f1293bf6750b5bb21ad83eadd
oleksandrchekalenko <whibleykevin@gmail.com>
2020-12-25 22:02:49 +0200

    Simple non terminal lambdas - used Double.sum() == Double::sum

--- src/simple/Test.java
+++ src/simple/Test.java
@@ -5,13 +5,13 @@ public class Test {
         Operations operations = new Operations() {
             @Override
             public double getResult(double d1, double d2) {
-                return d1 + d2;
+                return Double.sum(d1, d2);
             }
         };
 
         System.out.println(operations.getResult(2.0, 2.0));
 
-        Operations lambda = (d1, d2) -> d1 + d2;
+        Operations lambda = Double::sum;
         System.out.println(lambda.getResult(3.0, 3.0));
     }
 }
